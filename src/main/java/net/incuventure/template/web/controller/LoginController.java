4dd6fab993a9c879687c3558adde4227803e9ef1
Jett Gamboa <jettgamboa@gmail.com>
2014-10-15 10:08:17 +0800

    added external messages

--- src/main/java/net/incuventure/template/web/controller/LoginController.java
+++ src/main/java/net/incuventure/template/web/controller/LoginController.java
@@ -2,8 +2,12 @@ package net.incuventure.template.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/login")
@@ -12,13 +16,18 @@ public class LoginController {
     @RequestMapping(method = RequestMethod.GET)
     public String index(ModelMap model) {
 
+        Map loginForm = new HashMap();
+
+        loginForm.put("userid","test");
+
         model.addAttribute("message", "show accounting stuff");
+        model.addAttribute("loginForm", loginForm);
 
         return "login/login";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String login(ModelMap model) {
+    public String login(@ModelAttribute(value="loginForm") ModelMap model) {
 
         System.out.println(model.toString());
         return "login/login";
